0c0f329e7c84e9cd77293c99289d5442393f9364
Pratim Mehta <144026141+Blazeboss123123@users.noreply.github.com>
2025-10-08 22:22:18 +0100

    Update peerproject1.py

--- peerproject1.py
+++ peerproject1.py
@@ -1,35 +1,50 @@
-class User:
+class User:            #creates a user class for user information
     def __init__(self, username, birth):
         self.username = username
         self.birth = birth
 
-class Artist:
-    def __init__(self, name,length,singer, genre=None):
+class Artist:#creates an artist class for artist information as well as formating purposes
+    def __init__(self, name, length, singer, genre=None):
         self.name = name
+        self.length = length
         self.singer = singer
         self.genre = genre
-        self.length = length
 
-def preferences():
-   
-    FavArtist = input("Enter your favorite artist: ")
-    FavGenre = input("Enter your favorite genre: ")
-    print(f"Favorite Artist: {FavArtist}, Favorite Genre: {FavGenre}")
+def preferences():#function to get user preferences
+    fav_artist = input("Enter your favorite artist: ")
+    fav_genre = input("Enter your favorite genre: ")
+    print(f"Favorite Artist: {fav_artist}, Favorite Genre: {fav_genre}")
+    return fav_artist, fav_genre
 
+# Read artist data safely incase it doesnt exist it wont run with an error
+try:
+    with open("savedlibary.txt") as file:#file name and file needs to be created
+        data = [line.strip().split(",") for line in file]#splitting the file
+        data = [Artist(name, length, singer, genre) for name, length, singer, genre in data]#branding each item into its specified catgeory
+except FileNotFoundError:
+    print("No saved library found. Starting with an empty one.")
+    data = []
 
-data = [line.strip().split(",") for line in open("savedlibary.txt")]
-data = [Artist(name, genre,length,singer) for name, genre,length,singer in data]
-   
+# Get user info
+name = input("Enter your name: ")#simple user details
+birth = input("Enter your date of birth: ")
+user = User(name, birth)
 
+# Get preferences
+fav_artist, fav_genre = preferences()
+optionA = input("Would you like to see your songs sorted A-Z? (Yes/No): ").strip()
+if data and optionA.lower()=="yes":
+    song_titles = sorted([artist.name for artist in data])#reads artists name from data
+    print("\nSongs in your library (A–Z):")
+    for title in song_titles:#loops in order to print out all the possible titles in the file
+        print(f" - {title}")
+else:
+    print("\nNo songs in your library yet.")#checks when there is nothing put in
 
-Name = input("Enter your name: ")
-Date = input("Enter your date of birth: ")
-preferences()
-uncertain = True
-while uncertain:
+# Ask for confirmation
+while True:#this could be made better with functions
     question = input("Are you sure about your preferences? (yes/no): ").strip().lower()
-    if question == 'yes': uncertain = False
-    elif question == 'no': preferences()
-    else: print("Please answer with 'yes' or 'no'.")
-
-
+    if question == "yes":break
+    elif question == "no":fav_artist, fav_genre = preferences()
+    else:
+        print("Please answer with 'yes' or 'no'.")
